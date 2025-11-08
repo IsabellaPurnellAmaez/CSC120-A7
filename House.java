cfc6518a81e09a9071762bafaf09a573005aa313
Isabella Purnell-Amaez <ipurnellamaez@smith.edu>
2025-11-08 15:00:14 -0500

    Done with HW

--- House.java
+++ House.java
@@ -2,11 +2,21 @@
 
 import java.util.ArrayList;
 
+/**
+ * Class contains information for creating a house. 
+ */
 public class House extends Building implements HouseRequirements{
 
   private ArrayList<Student> residents; // The <Student> tells Java what kind of data we plan to store IN the ArrayList
   private boolean hasDiningRoom; 
 
+  /**
+   * House constructor
+   * @param name Name of House
+   * @param address Address of House
+   * @param nFloors Number of floors of house
+   * @param hasDiningRoom whether the house has a dining room
+   */
   public House(String name, String address, int nFloors, boolean hasDiningRoom) {
     super(name, address, nFloors);
     this.residents = new ArrayList<>();
@@ -14,35 +24,56 @@ public class House extends Building implements HouseRequirements{
 
     System.out.println("You have built a house: 🏠");
   }
+
+  /**
+   * Says whether the house has a dining room
+   * @return T/F: yes or no dining hall
+   */
   public boolean hasDiningRoom(){
     return this.hasDiningRoom;
   } 
 
+  /**
+   * Says the number of residents in the house (returns size of the list of residents)
+   * @return number of residents 
+   */
   public int nResidents(){
     return this.residents.size();
   }
 
-  public void moveIn(Student s){
-    if (!this.residents.contains(s)){
-      this.residents.add(s);
-    }
-    else{
-      System.out.println(s.getName() + " already lives here! Can't move in!");
-    }
-  } 
+  /**
+   * Says whether or not a student can move in and adds them to the list of residents if they can move in
+   * @param s is the student who is trying to move in
+   */
+  public void moveIn(Student s) {
+    if(!this.residents.contains(s)){
+        this.residents.add(s);
+      }else{
+        throw new RuntimeException(s.getName() + " already lives here, cannot move in.");
+      }
+    } 
 
-
+  /**
+   * Says whether or not a student can move out. If they don't live in the house, they cannot move out.
+   * Otherwise, they are removed from the list of residens
+   * @param s Student trying to move out.
+   * @return the Student moving out. If no one is moving out, an error gets thrown. 
+   */
   public Student moveOut(Student s){
     if (this.residents.contains(s)){
       this.residents.remove(s);
       return s;
     }
     else{
-      System.out.println(s.getName() + " does not live here! Can't be removed");
-      return s;
+      throw new RuntimeException(s.getName() + " does not live here, cannot be removed.");
     }
   } 
-
+  
+  /**
+   * Says whether or not a student lives in the house
+   * @param s Student that the method checks if they are a resident
+   * @return T/F: are they a resident 
+   */
   public boolean isResident(Student s){
     if (this.residents.contains(s)){
       return true;
--- reflection.md
+++ reflection.md
@@ -1,4 +1,11 @@
 Use this file to record your reflection on this assignment.
 
 - What are your initial impressions of how `Inheritance` affects your programming?
+
+I thought it was really helpful and easy to pick up. It makes it a lot simpler to code a lot of similar objects by making a base for all of them just one time and then adding specific attributes and methods rather than starting from scratch every single time. 
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+I had the hardest time with library because I didn't really know much about how to implement hashmaps. I think it came up a little in previous homeworks but I was not super familiar with the methods I needed to use. I also struggled a little bit with Cafe restock. I chose to have it just restock each thing when it runs out instead of restocking everything every time. 
+
+I also think some of my exeptions will work but will stop the program. I tried to implement try catch blocks but I was having a hard time. This is because the requirements for the class doesn't throw an exception. 
